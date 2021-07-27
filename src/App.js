ac369c533ad0f84f423d5c36445fd1e1ce14e461
harshizoe <harsh@izoe.in>
2021-07-27 16:22:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react'
+import axios from 'axios'
+import  Posts from './components/Posts'
+import  Pagination from './components/Pagination'
 import './App.css';
 
-function App() {
+const App =()=>{
+  const[posts,setPosts]= useState([]);
+  const [loading,setLoading]=useState(false);
+  const[currentPage,setCurrentPage]=useState(1);
+  const[postsPerPage]=useState(3);
+  useEffect(() => {
+  const fetchPosts=async()=>{
+    setLoading(true);
+    const res = await axios.get('https://reqres.in/api/users?page=2');
+    
+    setPosts(res.data.data);
+    console.log(res);
+   
+    setLoading(false);
+  };
+  
+  fetchPosts();
+  }, []);
+const indexOfLastPost = currentPage * postsPerPage;
+const indexOfFirstPost = indexOfLastPost-postsPerPage;
+const currentPosts = posts.slice(indexOfFirstPost,indexOfLastPost)
+const paginate = pageNumber=>setCurrentPage(pageNumber);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1 className="text-primary mb-3">My App</h1>
+    <Posts posts={currentPosts} loading={loading}/>
+    <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} paginate={paginate}/>
     </div>
   );
-}
 
+  };
 export default App;
